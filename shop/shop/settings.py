0dea55f6e82e7ba76409c4f9b703839b7e110c33
Fu Zhaohui <Fu Zhaohui@FuZhaohui-PC>
2012-12-14 13:22:00 +0800

    更新中文显示与用户密码admin/admin

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -92,12 +92,16 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    # Language i18n Simple Chinese Language
+    'django.middleware.locale.LocaleMiddleware', 
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
